207db95eef5e7106d2dc242ad6e0c2b7e50ae4a1
PiyushKhanna30 <khanna.piyush30@gmail.com>
2019-09-10 19:13:39 +0530

    created and migrated model

--- urlshorten/shortener/models.py
+++ urlshorten/shortener/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Url(models.Model):
+	short_id=models.SlugField(max_length=6,primary_key=True)
+	http_url=models.URLField(max_length=200)
+	publish_date=models.DateTimeField(auto_now=True)
+	count=models.IntegerField(default=0)
+	class Meta:
+		verbose_name_plural="Urls"
+	def __str__(self):
+		return (self.http_url)
