6dc5b2304f2720c78a750e273d12d449564d04e5
PiyushKhanna30 <khanna.piyush30@gmail.com>
2019-09-12 16:46:00 +0530

    Validating Urls

--- urlshorten/shortener/views.py
+++ urlshorten/shortener/views.py
@@ -3,22 +3,31 @@ from .models import Url
 from django.http import HttpResponse
 import string,random
 from django.conf import settings
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
 
 def shorten_url(request):
 	response_data = {}
 	if request.method == 'POST':
 		http_url=request.POST.get('http_url')
 		if request.POST.get('http_url'):
+			val = URLValidator()
 			try:
-				http_url = Url.objects.get(http_url=http_url)
-				short_id = http_url.short_id
-			except:
-				short_id=get_short_code()
-				temp=Url(http_url=http_url,short_id=short_id)
-				temp.save()
-			response_data["f_url"]=http_url		
-			response_data["url"]=settings.SITE_URL+"/"+short_id		
-			return render(request=request,template_name='shortener/generated.html',context={'short_url':response_data["url"],'full_url':response_data["f_url"]})
+				val(http_url)
+				try:
+					http_url = Url.objects.get(http_url=http_url)
+					short_id = http_url.short_id
+				except:
+					short_id=get_short_code()
+					temp=Url(http_url=http_url,short_id=short_id)
+					temp.save()
+				response_data["f_url"]=http_url		
+				response_data["url"]=settings.SITE_URL+"/"+short_id		
+				return render(request=request,template_name='shortener/generated.html',context={'short_url':response_data["url"],'full_url':response_data["f_url"]})
+			except ValidationError:
+				e="Please enter a valid url."
+				return render(request=request,template_name='shortener/index.html',context={'response_data':e})
 	return render(request=request,template_name='shortener/index.html',context={'response_data':None})
 
 def get_url(request,short_id):
@@ -40,3 +49,5 @@ def get_short_code():
 			temp1=Url.objects.get(pk=short_id)
 		except:
 			return short_id
+
+
